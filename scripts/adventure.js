4319655363711af795e59149c063cdd4e682e68f
Akbar Mirza <akbarbmirza@gmail.com>
2015-08-13 17:33:33 -0500

    Add handlebars templating code

--- scripts/adventure.js
+++ scripts/adventure.js
@@ -1,3 +1,35 @@
+var source = $('#card-template').html();
+var template = Handlebars.compile(source);
+var data = {
+  'welcome': {
+    title: 'Welcome',
+    body: '<p>You wake up in a room! You see a door and a window. Which do you use?</p>',
+    choice: [
+      { next: 'window', text: 'Go to the Window'},
+      { next: 'door', text: 'Open the door'}
+    ]
+  },
+  'window': {
+    id: 'window',
+    title: 'The Window',
+    body: '<p>You see a beautiful world outside!</p>',
+    choice: [
+      { next: 'sleep', text: 'Go to sleep'},
+      { next: 'smile', text: 'Smile'}
+    ]
+  }
+};
+
+Handlebars.registerHelper('list', function (context, options) {
+  var ret = '<ul>';
+
+  for (var i = 0, len = context.length; i < len; i++) {
+    ret += '<li>' + options.fn(context[i]) + '</li>';
+  }
+
+  return ret + '</ul>';
+});
+
 /* Start Smooth Scroll
 Source for Code Snippet:
 https://css-tricks.com/snippets/jquery/smooth-scrolling/
